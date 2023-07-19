e82fbb08e996df957bdc3aae0e574a83ab4a865c
Nikolai2038 <nikolai2038@yandex.ru>
2023-07-19 09:20:44 +0300

    Fix wrong changes in a93de945

--- move.ps1
+++ move.ps1
@@ -1,18 +1,17 @@
-
 param(
     # WSL name to move
-    [Parameter(Mandatory=$true)]
-    [string]${movingWSLName} = "",
+    [Parameter(Mandatory = $true)]
+    [string]${movingWSLName},
+
     # Full path to new directory to store WSL folder
-    [Parameter(Mandatory=$true)]
-    [string]${targetWSLDir} = ""
+    [Parameter(Mandatory = $true)]
+    [string]${targetWSLDir}
 )
 
-if (!(${movingWSLName}) -or !(${targetWSLDir})) {
-    Write-Error "Usage: move.ps1 <WSL name to move> <full path to new directory to store WSL folder>"
-    Write-Output "Example: move.ps1 `"docker-desktop-data`" `"D:\WSL`""
-    Exit 1
-}
+Write-Output "Current WSL list:"
+wsl -l -v
+# Exit if was error
+if (${LASTEXITCODE} -ne 0) { Exit ${LASTEXITCODE} }
 
 # Full path to new WSL folder
 $wslNewPath = "${targetWSLDir}\${movingWSLName}"
@@ -26,30 +25,45 @@ $wslTempArchivePath = "${dirToTempStoreArchives}\${movingWSLName}.tar"
 # ========================================
 # Moving WSL data
 # ========================================
-# 1. Stop WSLs
+Write-Output "Shutting down WSL..."
 wsl --shutdown
 # Exit if was error
 if (${LASTEXITCODE} -ne 0) { Exit ${LASTEXITCODE} }
+Write-Output "Shutting down WSL: done!"
 
-# 2. Save WSL data to archive
+Write-Output ""
+Write-Output "Saving WSL data to archive..."
 wsl --export "${movingWSLName}" "${wslTempArchivePath}"
 # Exit if was error
 if (${LASTEXITCODE} -ne 0) { Exit ${LASTEXITCODE} }
+Write-Output "Saving WSL data to archive: done!"
 
-# 3. Now remove WSL data
+Write-Output ""
+Write-Output "Removing WSL data..."
 wsl --unregister "${movingWSLName}"
 # Exit if was error
 if (${LASTEXITCODE} -ne 0) { Exit ${LASTEXITCODE} }
+Write-Output "Removing WSL data: done!"
 
-# 4. Importing the distribution back to WSL, but now to a new location
+Write-Output ""
+Write-Output "Importing the distribution back to WSL, but now to a new location..."
 wsl --import "${movingWSLName}" "${wslNewPath}" "${wslTempArchivePath}" --version 2
 # Exit if was error
 if (${LASTEXITCODE} -ne 0) { Exit ${LASTEXITCODE} }
+Write-Output "Importing the distribution back to WSL, but now to a new location: done!"
 
-# 5. Remove temp archive
+Write-Output ""
+Write-Output "Removing temp archive..."
 Remove-Item "${wslTempArchivePath}"
 # Exit if was error
 if (${LASTEXITCODE} -ne 0) { Exit ${LASTEXITCODE} }
+Write-Output "Removing temp archive: done!"
 # ========================================
+
+Write-Output ""
+Write-Output "New WSL list:"
+wsl -l -v
+# Exit if was error
+if (${LASTEXITCODE} -ne 0) { Exit ${LASTEXITCODE} }
 
 Exit 0
